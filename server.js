7ebc05c27c6dd68995d4159c9da17b55669de3ef
ANNAPUREDDY VINAY KUMAR REDDY <vinayavkr06@gmail.com>
2023-11-16 20:19:11 +0530

    Add user details

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ const app=express()
 app.get('/api/get',(req,res)=>{
     res.send("Hello, world!")
 })
-
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"satyam",
+            age:22,
+            contact:12254
+        }
+    })
+});
 
 app.listen(8000,()=>{
     console.log("Listening on")
